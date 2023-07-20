835b26407f9d4fac07759c36e0310477d2ed60fc
Asus <Asus@Lalit09>
2023-07-20 12:25:23 +0530

    first commit

---
+++ dayone/src/org/tnsif/introduction/UserInputDemo.java
@@ -0,0 +1,21 @@
+
+//program to demonstrate the user input
+
+package org.tnsif.introduction;
+import java.util.Scanner;
+
+public class UserInputDemo {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner (System.in);
+		System.out.println("Enter the value of number");
+			int num=s.nextInt();
+		System.out.println(" The value of num is:"+num);
+		System.out.println("Enter the value of floatnum");
+		float num1=s.nextFloat();
+	    System.out.println(" The value of floatnum is:"+num1);
+	    
+	    System.out.println("Enter The value of char);
+	}
+
+}
