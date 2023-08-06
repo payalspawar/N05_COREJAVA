54a7cc1fe1f6dad6254832b1a773689574a80007
Asus <Asus@Lalit09>
2023-08-06 16:14:07 +0530

    two commit

---
+++ daytwo/src/org/tnsif/jumping/BreakExecutor.java
@@ -0,0 +1,16 @@
+package org.tnsif.jumping;
+
+public class BreakExecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=1;1<=10;i++)
+{
+	if(i==5)
+		break;
+	else
+		System.out.println(i +" ");
+}
+	}
+
+}
